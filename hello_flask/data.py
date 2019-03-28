592757cc4094ac0e0963011cdd4826912bb120cd
chartswapdev <lgonzalez@chartswap.com>
2019-03-28 10:56:15 -0500

    Commit of application that has login support -- sponsored by Hunter Application connects to hosted mysql database that Donald can populate

---
+++ hello_flask/data.py
@@ -0,0 +1,53 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus felis quam, a feugiat tellus '
+                     'tempus quis. Nunc porta, lorem ut lacinia interdum, justo massa aliquet dui, vitae tempor sapien '
+                     'ipsum vitae lorem. Suspendisse commodo accumsan ex, non dapibus turpis bibendum at. Fusce nec '
+                     'faucibus neque. Integer et velit a lectus dictum pulvinar. Integer facilisis euismod massa, quis '
+                     'sollicitudin est interdum a. Sed cursus ex ut lorem ultricies mollis. Sed maximus felis odio, '
+                     'viverra imperdiet orci placerat a.'
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus felis quam, a feugiat tellus '
+                    'tempus quis. Nunc porta, lorem ut lacinia interdum, justo massa aliquet dui, vitae tempor sapien '
+                    'ipsum vitae lorem. Suspendisse commodo accumsan ex, non dapibus turpis bibendum at. Fusce nec '
+                    'faucibus neque. Integer et velit a lectus dictum pulvinar. Integer facilisis euismod massa, quis '
+                    'sollicitudin est interdum a. Sed cursus ex ut lorem ultricies mollis. Sed maximus felis odio, '
+                    'viverra imperdiet orci placerat a.'
+
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus felis quam, a feugiat tellus '
+                    'tempus quis. Nunc porta, lorem ut lacinia interdum, justo massa aliquet dui, vitae tempor sapien '
+                    'ipsum vitae lorem. Suspendisse commodo accumsan ex, non dapibus turpis bibendum at. Fusce nec '
+                    'faucibus neque. Integer et velit a lectus dictum pulvinar. Integer facilisis euismod massa, quis '
+                    'sollicitudin est interdum a. Sed cursus ex ut lorem ultricies mollis. Sed maximus felis odio, '
+                    'viverra imperdiet orci placerat a.',
+            'author': 'hunter',
+            'create_date': '04/28/2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus felis quam, a feugiat tellus '
+                    'tempus quis. Nunc porta, lorem ut lacinia interdum, justo massa aliquet dui, vitae tempor sapien '
+                    'ipsum vitae lorem. Suspendisse commodo accumsan ex, non dapibus turpis bibendum at. Fusce nec '
+                    'faucibus neque. Integer et velit a lectus dictum pulvinar. Integer facilisis euismod massa, quis '
+                    'sollicitudin est interdum a. Sed cursus ex ut lorem ultricies mollis. Sed maximus felis odio, '
+                    'viverra imperdiet orci placerat a.',
+            'author': 'eric',
+            'create_date': '01/28/2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus felis quam, a feugiat tellus '
+                    'tempus quis. Nunc porta, lorem ut lacinia interdum, justo massa aliquet dui, vitae tempor sapien '
+                    'ipsum vitae lorem. Suspendisse commodo accumsan ex, non dapibus turpis bibendum at. Fusce nec '
+                    'faucibus neque. Integer et velit a lectus dictum pulvinar. Integer facilisis euismod massa, quis '
+                    'sollicitudin est interdum a. Sed cursus ex ut lorem ultricies mollis. Sed maximus felis odio, '
+                    'viverra imperdiet orci placerat a.',
+            'author': 'shauna',
+            'create_date': '09/28/2011'
+        }
+    ]
+    return articles
